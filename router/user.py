8143fa745a1be279d2fab61ddbbf43739bc5de7a
Alex G <alex.alg.grecu@gmail.com>
2023-03-21 18:57:12 +0200

    added UserDisplay to schemas

--- router/user.py
+++ router/user.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from schemas import UserBase
+from schemas import UserBase, UserDisplay
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_user
@@ -10,6 +10,6 @@ router = APIRouter(
 )
 
 #Create user
-@router.post('/')
+@router.post('/', response_model=UserDisplay)
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
