c219f533f276d2148ba829c7a1460d6447904571
Alex G <alex.alg.grecu@gmail.com>
2023-03-21 17:41:44 +0200

    orm functionality

---
+++ db/db_user.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm.session import Session
+from schemas import UserBase
+from db.models import DbUser
+
+def create_user(db: Session, request: UserBase):
+    new_user = DbUser(
+        username = request.username,
+        email = request.email,
+        password = ''
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
