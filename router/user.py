89b18585b0d92b5031d62dbbc64585eae8557b14
Alex G <alex.alg.grecu@gmail.com>
2023-03-21 20:44:54 +0200

    added routes to get all and get single user

--- router/user.py
+++ router/user.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from schemas import UserBase, UserDisplay
+from typing import List
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_user
@@ -13,3 +14,13 @@ router = APIRouter(
 @router.post('/', response_model=UserDisplay)
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
+
+#Real all users
+@router.get('/user', response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+    return db_user.get_all_users(db)
+
+#Read one user
+@router.get('/{id}', response_model=UserDisplay)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return db_user.get_user(db, id)
